54131043f572a864e4ae208ff890cc2ff21038f4
Daviny Vidal <daviny@vidal.dev.br>
2023-12-01 12:41:21 -0300

    add

--- src/app/components/molecules/footer/Footer.tsx
+++ src/app/components/molecules/footer/Footer.tsx
@@ -4,11 +4,14 @@ import React from "react";
 import styled from "styled-components";
 
 const Conteiner = styled.div`
-margin-top: 3rem`;
+  margin-top: 3rem @media (max-width: 1265px) {
+    width: 100%;
+  }
+`;
 
 const Content = styled.div`
   background: #5b4e5f;
-  a{
+  a {
     text-decoration: none;
     color: #000;
     background: #fff;
@@ -24,31 +27,30 @@ const BugText = styled.div`
   height: 16rem;
   padding-top: 4rem;
   h3 {
-    font-family: Rubik,Arial,Helvetica,sans-serif;
+    font-family: Rubik, Arial, Helvetica, sans-serif;
     font-size: 1.4rem;
     color: #fff;
     background: #5b4e5f;
     color: #f1802d;
-    
   }
   p {
-    font-family: Rubik,Arial,Helvetica,sans-serif;
+    font-family: Rubik, Arial, Helvetica, sans-serif;
     font-size: 1.4rem;
     line-height: 1.5;
     color: #fff;
     background: #5b4e5f;
     padding: 0 1rem;
   }
-  div{
+  div {
     background: #5b4e5f;
     margin: 0;
   }
   @media (max-width: 1265px) {
     display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-  padding-top: 0;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    padding-top: 0;
   }
 `;
 
@@ -59,7 +61,7 @@ const Note = styled.div`
     background: #8e7c93;
     padding: 1rem 0;
     text-align: center;
-    font-family: Rubik,Arial,Helvetica,sans-serif;
+    font-family: Rubik, Arial, Helvetica, sans-serif;
     font-size: 1.4rem;
     line-height: 1.5;
   }
@@ -73,12 +75,19 @@ const Footer: React.FC = () => {
           <h3>Sobre este site</h3>
           <div>
             <p>Construído com NextJS. </p>
-            <p>Se você encontrar um bug, registre um  <a href="https://github.com/davinyleticia/mypage/issues">issue</a>. Obrigada! </p>
+            <p>
+              Se você encontrar um bug, registre um{" "}
+              <a href="https://github.com/davinyleticia/mypage/issues">issue</a>
+              . Obrigada!{" "}
+            </p>
             <p>Este site não coleta nenhum dado pessoal.</p>
           </div>
         </BugText>
         <Note>
-          <p>Feito com amor por Daviny Letícia 2023 @ <a href="https://github.com/davinyleticia">Github</a></p>
+          <p>
+            Feito com amor por Daviny Letícia 2023 @{" "}
+            <a href="https://github.com/davinyleticia">Github</a>
+          </p>
         </Note>
       </Content>
     </Conteiner>
