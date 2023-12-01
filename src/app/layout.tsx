4155771b97b8a2fa8f23686eee862a2cc427ff5d
Daviny Vidal <daviny@vidal.dev.br>
2023-12-01 12:01:29 -0300

    remove font google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import Footer from "./components/molecules/footer/Footer";
 import Header from "./components/molecules/header/Header";
 import "./globals.css";
 import StyledComponentsRegistry from "./lib/registry";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Daviny Letícia",
@@ -19,10 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt">
-      <StyledComponentsRegistry className={inter.className}>
+      <StyledComponentsRegistry>
         <Header />
         {children}
-        <Footer/>
+        <Footer />
       </StyledComponentsRegistry>
     </html>
   );
