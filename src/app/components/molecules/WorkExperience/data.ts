d6dbc7cb3da4afcb9f42490f669ea81df5a01502
Daviny Vidal <daviny@vidal.dev.br>
2023-12-01 12:14:40 -0300

    add experience

--- src/app/components/molecules/WorkExperience/data.ts
+++ src/app/components/molecules/WorkExperience/data.ts
@@ -10,4 +10,33 @@ export const experience = [{
     description: "Empresa do ramo de educação técnicos",
     worker:"Atuei com front-end no desenvolvimento do ERP interno da instituição",
     img:"/logotipo_proz.svg"
-},]
+},
+{
+    name: "DIO.me | Front-end e Django",
+    span: "Online - Brasil | abri de 2021 - abri 2023",
+    description: "Empresa do ramo de educação e bootcamps",
+    worker:"Responsável pela evolução do produto, plataforma e experiências digitais atuando no time de Product, Technology and Data. Atuando diariamente com as linguagens Python, AWS, React, TypeScript, Redux, Hooks.",
+    img:"/logotipo_proz.svg"
+},
+{
+    name: "Codivas | Front-end",
+    span: "Freelance - Brasil | fev de 2021 - mar 2021",
+    description: "Coletivo de Mulheres voltado para empoderamento e empregabilidade",
+    worker:"tuei no desenvolvimento Front-end freelance no projeto da empresa Bio Doctor, a minha parte ficou recarregada de fazer rebranding visual do site, utilizando CSS, HTML e JS.",
+    img:"/coletivo_codivas_logo.jpeg"
+},
+{
+    name: "RHS | Back-end PHP",
+    span: "Online - Brasil | ago de 2020 - jan 2021",
+    description: "Administração pública, defesa e seguridade social, voltado para licitações",
+    worker:"manutenção scripts PHP (laravel) para automações processos internos",
+    img:"/rhs_licitaes_logo.jpeg"
+},
+{
+    name: "Braz Cubas | Técnica de Laboratório",
+    span: "Mogi das Cruzes - Brasil | Fev de 2015 - ago 2020",
+    description: "Universidade",
+    worker:"Mentoria em análise de dados dos projetos dos alunos, participação junto com os professores em aulas práticas de laboratório. ",
+    img:"/brazcubasoficial_logo.jpeg"
+},
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@
 
 import styled from "styled-components";
 import CurriculumButton from "./components/atom/CurriculumButton";
-import Education from "./components/Education/Education";
+import Education from "./components/molecules/Education/Education";
 import Contacts from "./components/molecules/Contacts/Contacts";
 import Skills from "./components/molecules/Skills/Skills";
 import WhoAmI from "./components/molecules/WhoAmI/WhoAmI";
 import WorkExperience from "./components/molecules/WorkExperience/WorkExperience";
-import Projects from "./components/Projects/Projects";
-import Talks from "./components/talks/Talks";
+import Projects from "./components/molecules/Projects/Projects";
+import Talks from "./components/molecules/talks/Talks";
 
 const Container = styled.div`
   width: 60rem;
