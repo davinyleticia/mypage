dfc0bf1356cfc80e3396c0ea670cc7a6494b2f65
Daviny Vidal <daviny@vidal.dev.br>
2023-12-05 21:59:10 -0300

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,33 @@
-// Import necessary modules and components
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import Head from "next/head";
 import Footer from "./components/organelle/footer/Footer";
 import Header from "./components/organelle/header/Header";
 import "./globals.css";
 import StyledComponentsRegistry from "./lib/registry";
 
-// Define metadata for the website
 export const metadata: Metadata = {
   title: "Daviny Letícia",
   description: "My professional website",
 };
 
-// Define the RootLayout component
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <Head>
-        {/* Include metadata, charset, viewport settings, and favicon */}
+    <html lang="pt">
+        <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="./favicon.svg" />
       </Head>
-      
-      {/* Use StyledComponentsRegistry to wrap the content */}
       <StyledComponentsRegistry>
-        {/* Render the Header component */}
         <Header />
-
-        {/* Render the content provided by the pages */}
         {children}
-
-        {/* Render the Footer component */}
         <Footer />
       </StyledComponentsRegistry>
-    </>
+    </html>
   );
 }
