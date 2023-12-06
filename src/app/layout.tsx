05885e48251af85c710e9fe3fdfe13eca36cf257
Daviny Vidal <daviny@vidal.dev.br>
2023-12-05 21:51:04 -0300

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Head from 'next/head';
 import Footer from "./components/organelle/footer/Footer";
 import Header from "./components/organelle/header/Header";
 import "./globals.css";
@@ -16,6 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt">
+        <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="shortcut icon" href="./favicon.svg" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
+      </Head>
       <StyledComponentsRegistry>
         <Header />
         {children}
