f422df54955495b1c8889e4cd083013f89483cde
Daviny Letícia Vidal <daviny@vidal.dev.br>
2023-12-29 14:36:16 -0300

    aDDD

--- src/app/components/atom/CurriculumButton.tsx
+++ src/app/components/atom/CurriculumButton.tsx
@@ -3,9 +3,11 @@ import { RxActivityLog } from "react-icons/rx";
 import styled from "styled-components";
 
 const Conteiner = styled.div`
-  width: 14rem;
-  margin-top: 7rem;
-  z-index: 1000;`
+width: 14rem;
+z-index: 1000;
+position: static;
+bottom: 0;
+`
 
 const Link = styled.a`
   display: flex;
@@ -13,7 +15,8 @@ const Link = styled.a`
   align-items: center;
   width: 8rem;
   color: #000;
-  border: solid 1px #ea370f;
+  background: #f1802d;
+  border: solid 1px #f1802d;
   padding: 1rem;
   border-radius: 5%; /* Add the missing semicolon here */
   :hover {
