c12c19a30dd68df5d9b74f435fad56ff19e5b09d
Daviny Letícia Vidal <daviny@vidal.dev.br>
2024-01-04 23:29:05 -0300

    add

--- src/app/components/atom/CurriculumButton.tsx
+++ src/app/components/atom/CurriculumButton.tsx
@@ -8,21 +8,28 @@ const Conteiner = styled.div`
     z-index: 1000;
     width: 100%;
     p{
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      border-radius: 3rem; /* Add the missing semicolon here */
-      background: #fff;
-      border: solid 1px #fff;
-      width: 9rem;
-      padding: 1rem;
+      color: #FFF;
+  border: 2px solid #fff;
+  border-radius: 3rem;
+  padding: 18px 36px;
+  display: inline-block;
+  font-family: "Lucida Console", Monaco, monospace;
+  font-size: 14px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  box-shadow: inset 0 0 0 0 #fff;
+  -webkit-transition: ease-out 0.4s;
+  -moz-transition: ease-out 0.4s;
+  transition: ease-out 1s;
     }
 `
 
 const Link = styled.a`
   color: #000;
   :hover {
-    text-decoration: underline; /* Fix the typo here (line instead of underline) */
+    text-decoration: none;
+    box-shadow: inset 400px 0 0 0 #fff;
+    color: #000;
   }
 `;
 
