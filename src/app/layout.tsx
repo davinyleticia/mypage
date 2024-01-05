917ba2af51990a272e60ee1c83d3c67b79e70690
Daviny Letícia Vidal <daviny@vidal.dev.br>
2024-01-05 00:12:25 -0300

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="shortcut icon" href="./favicon.svg" />
+        <link rel="con" href="favicon.svg" sizes="any" type="image/svg+xml" />
       </Head>
       <StyledComponentsRegistry>
         <Menu/>
